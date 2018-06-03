77f01936efd752c0347c4f017cfd3bac0fcbf7d2
mgechev <mgechev@gmail.com>
2018-06-02 18:06:46 -0700

    Refactor tests

--- __tests__/evaluator.spec.ts
+++ __tests__/evaluator.spec.ts
@@ -4,17 +4,16 @@ import { Symbols } from '../symbols';
 import { readFileSync } from 'fs';
 import { MetadataEntry } from '../schema';
 
-const file = ts.createSourceFile(
+const sample = ts.createSourceFile(
   'sample.ts',
   readFileSync('./__tests__/fixtures/sample.ts').toString(),
   ts.ScriptTarget.ES2015
 );
-const meta = new Map<MetadataEntry, ts.Node>();
-const evaluator = new Evaluator(new Symbols(file), meta);
+const evaluator = new Evaluator(new Symbols(sample), new Map<MetadataEntry, ts.Node>());
 
 describe('Evaluator', () => {
   it('should work', () => {
-    const arrow = <ts.ArrowFunction>(<ts.VariableStatement>file.statements[4]).declarationList.declarations[0]
+    const arrow = <ts.ArrowFunction>(<ts.VariableStatement>sample.statements[4]).declarationList.declarations[0]
       .initializer;
     const node = (<ts.CallExpression>arrow.body).arguments[0];
     expect(evaluator.evaluateNode(node)).toBe('/prefix/lazy');
